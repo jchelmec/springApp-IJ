e23305c1c11e943922e7140248175a9fd82e481f
Jarek Chelmecki <jchelmec@gmail.com>
2020-10-30 22:10:05 +0100

    First Commit

---
+++ src/main/java/pl/jchelmec/springAppIJ/SpringAppIjApplication.java
@@ -0,0 +1,14 @@
+package pl.jchelmec.springAppIJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppIjApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringAppIjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jchelmec/springAppIJ/SpringAppIjApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jchelmec.springAppIJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppIjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
